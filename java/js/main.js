70b8b97ebb86ba5601711e056056276b6d0467e1
Lovepreet29 <54912545+Lovepreet29@users.noreply.github.com>
2019-09-20 14:41:30 -0600

    submit form

---
+++ java/js/main.js
@@ -0,0 +1,27 @@
+/*
+ * user field
+ * password field
+ * button
+  
+ 
+ * add the event to the button 
+ */
+
+ const fakeuser="love";
+ const fakepassword="4905";
+ 
+ const cardHolder = document.querySelector('.user-name');
+ const cardPassword = document.querySelector('.card-password');
+ const cardNumber = document.querySelector('.card-number');
+ const submitButton = document.querySelector('.verify-card');
+ 
+ submitButton.addEventListener('click', function(){
+
+    const card = (cardName.value);
+    if (fakeuser == name) {
+console.log("username is valid ")
+    }
+ else{
+console.log("username is not valid")
+ }
+ })
